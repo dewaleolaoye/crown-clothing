85448979c0cd48d5796fb5b026fe472578a511f9
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-10-06 17:25:05 +0100

    setup node-sass

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Homepage from './homepage.component';
+
+const App = () => {
+  return (
+    <>
+      <Homepage />
+    </>
+  );
+};
+
+export default App;
