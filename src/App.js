ef7d9a8bab9cfd0bbbc92dacd167f54596204047
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-10-07 11:41:31 +0100

    shop data file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './pages/homepage.styles.scss';
 import './App.css';
+
 import Homepage from './pages/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const App = () => {
   return (
     <>
       <Homepage />
+      <ShopPage />
     </>
   );
 };
