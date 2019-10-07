d64c1ed17cc0e7a957b8337c461444626ca3bc19
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-10-07 08:41:40 +0100

    update menu item withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import './pages/homepage.styles.scss';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Homepage from './pages/homepage.component';
 
+const HatsPage = () => (
+  <React.Fragment>
+    <h1>Hats page</h1>
+  </React.Fragment>
+);
+
 const App = () => {
   return (
-    <>
-      <Homepage />
-    </>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
   );
 };
 
