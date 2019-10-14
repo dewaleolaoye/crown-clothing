f285a6d6b0efa8575e7740799cd0f1d1ca723177
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-10-14 10:10:28 +0100

    redux action setup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './redux/store';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
